5c032ea9824d24c6e2e0fa15751272ab48d2a56c
Edgar Albalate <ealbalate@itbacking.com>
2023-01-26 14:49:28 +0100

    makes uses of modules

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
+use std::env::args;
+use std::path::PathBuf;
+
+use regex::Regex;
+
 #[derive(Debug)]
-struct Cli {
-    pattern: String,
-    path: std::path::PathBuf,
-}
+struct Cli { pattern: Regex, path: PathBuf }
 
 impl Cli {
     pub fn new(pattern: & str, path: & str) -> Cli {
         Cli {
-            pattern: String::from(pattern),
-            path: std::path::PathBuf::from(path)
+            pattern: Regex::new(pattern).expect("Invalid regex"),
+            path: PathBuf::from(path)
         }
     }
 }
 
 fn main() {
 
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+    let pattern = args().nth(1).expect("no pattern given");
+    let path = args().nth(2).expect("no path given");
+    println!(" - pattern: {0:?}\n - path: {1:?}", &pattern, &path);
     let args = Cli::new(&pattern, &path);
-    println!(" - pattern: {0:?}\n - path: {1:?}", args.pattern, args.path);
+    println!("Args built: {:?}", args);
 }
