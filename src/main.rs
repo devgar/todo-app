9b1f187cb0ab53ae384148a4d43dbca70b75a9a7
Edgar Albalate <ealbalate@itbacking.com>
2023-01-26 14:34:36 +0100

    Adds constructor

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,19 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+impl Cli {
+    pub fn new(pattern: & str, path: & str) -> Cli {
+        Cli {
+            pattern: String::from(pattern),
+            path: std::path::PathBuf::from(path)
+        }
+    }
+}
+
 fn main() {
 
     let pattern = std::env::args().nth(1).expect("no pattern given");
     let path = std::env::args().nth(2).expect("no path given");
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::new(&pattern, &path);
     println!(" - pattern: {0:?}\n - path: {1:?}", args.pattern, args.path);
 }
