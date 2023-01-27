18f60f2ff2478c1322672dfadfe862a52e6c6727
Edgar Albalate <dev.gardo@gmail.com>
2023-01-27 23:38:40 +0100

    Changes code to make a todo app

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,7 @@
-use std::env::args;
-use std::path::PathBuf;
+mod cli;
 
-use regex::Regex;
-
-#[derive(Debug)]
-struct Cli { pattern: Regex, path: PathBuf }
-
-impl Cli {
-    pub fn new(pattern: & str, path: & str) -> Cli {
-        Cli {
-            pattern: Regex::new(pattern).expect("Invalid regex"),
-            path: PathBuf::from(path)
-        }
-    }
-}
+use structopt::StructOpt;
 
 fn main() {
-
-    let pattern = args().nth(1).expect("no pattern given");
-    let path = args().nth(2).expect("no path given");
-    println!(" - pattern: {0:?}\n - path: {1:?}", &pattern, &path);
-    let args = Cli::new(&pattern, &path);
-    println!("Args built: {:?}", args);
+    cli::CommandLineArgs::from_args();
 }
