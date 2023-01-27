7eb98e4f86f743bff142b1034b1dbcc77a532c10
Edgar Albalate <dev.gardo@gmail.com>
2023-01-27 23:49:17 +0100

    Adds code to add tasks

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,3 +1,7 @@
+use std::fs::{File, OpenOptions};
+use std::path::PathBuf;
+use std::io::{Result, Seek, SeekFrom};
+
 use chrono::{serde::ts_seconds, DateTime, Local, Utc};
 use serde::{Deserialize, Serialize};
 
@@ -7,4 +11,29 @@ pub struct Task {
 
     #[serde(with = "ts_seconds")]
     pub created_at: DateTime<Utc>,
+}
+
+pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+    // Open the file.
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(journal_path)?;
+
+    // Consume the file's contents as a vector of tasks.
+    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+
+    // Rewind the file after reading from it.
+    file.seek(SeekFrom::Start(0))?;
+
+    // Write the modified task list back into the file.
+    tasks.push(task);
+    serde_json::to_writer(file, &tasks)?;
+
+    Ok(())
 }
