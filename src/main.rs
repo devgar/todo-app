863f21b6cf6e661668a3df91606595c3ef64135d
Edgar Albalate <dev.gardo@gmail.com>
2023-01-28 00:31:04 +0100

    Implements full program logic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
-mod cli;
-
 use structopt::StructOpt;
 
+mod cli;
+mod tasks;
+
+use cli::{Action::*, CommandLineArgs};
+use tasks::Task;
+
 fn main() {
-    println!("{:#?}", cli::CommandLineArgs::from_args());
+    // Get the command-line arguments.
+    let CommandLineArgs {
+        action,
+        journal_path,
+    } = CommandLineArgs::from_args();
+
+    let journal_path = journal_path.expect("Failed to find journal file");
+
+    match action {
+        Add { text } => tasks::add_task(journal_path, Task::new(text)),
+        List => tasks::list_tasks(journal_path),
+        Done { position } => tasks::complete_task(journal_path, position),
+    }
+    .expect("Failed to perform action")
 }
