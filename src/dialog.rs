5f8569e5bcec289e3c024bf3c20001c1e6b45a3f
Edgar Albalate <ealbalate@itbacking.com>
2023-01-31 14:10:25 +0100

    adds handler for ctrl+c during empty done action

--- src/dialog.rs
+++ src/dialog.rs
@@ -2,6 +2,9 @@ use std::io::{Result};
 use dialoguer::{theme::ColorfulTheme, FuzzySelect};
 
 pub fn ask<T: ToString>(items: &[T]) -> Result<usize> {
+
+    ctrlc::set_handler(move || std::process::exit(0))
+        .expect("Error setting Ctrl-C handler");
     let pos = FuzzySelect::with_theme(&ColorfulTheme::default())
         .with_prompt("What is Done?")
         .default(0)
